91df6a6e033bdabf4b3378a3e2e5840f8a13f54d
master00041 <master041amin@gmail.com>
2016-03-17 15:52:24 +0330

    Create chat.lua

---
+++ plugins/chat.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "با بابام چیکار داری جنده؟ 🙂🙃"
+  end
+return {
+  description = "botfather", 
+  usage = "admin name: ",
+  patterns = {
+    "^امین",
+  },
+  run = run
+}
+end
